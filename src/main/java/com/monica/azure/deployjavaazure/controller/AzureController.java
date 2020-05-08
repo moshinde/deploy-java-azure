1523d549545a56b260ce07dfdc5b83da887030f6
moshinde <31102278+moshinde@users.noreply.github.com>
2020-05-08 21:38:35 +0530

    updated for deployment object

--- src/main/java/com/monica/azure/deployjavaazure/controller/AzureController.java
+++ src/main/java/com/monica/azure/deployjavaazure/controller/AzureController.java
@@ -1,22 +1,29 @@
 package com.monica.azure.deployjavaazure.controller;
-
 import java.net.InetAddress;
-
 import javax.servlet.http.HttpServletRequest;
-
+import com.monica.azure.deployjavaazure.model.Deployment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AzureController {
+    @Autowired
+    BuildProperties buildProperties;
+       
     @RequestMapping(value="/gethost", method=RequestMethod.GET)
-	public String requestMethodName(final HttpServletRequest request) {
+	public Deployment requestMethodName(final HttpServletRequest request) {
         final String clientAddress=request.getServerName();
+        Deployment deployment = new Deployment();
         try{
             InetAddress serverAddress;
             serverAddress= InetAddress.getLocalHost();
-            return "Request is comming from : "+clientAddress+" for server with hostname/ip : "+serverAddress;
+            deployment.setClientAddress(clientAddress);
+            deployment.setServerAddress(serverAddress.toString());
+            deployment.setPackageVersion(buildProperties.getVersion());
+            return deployment;
         }catch(Exception e){
             e.printStackTrace();
         }
