52bace21c98f6c87e5e569edec0d7505bd198d51
EC2 Default User <ec2-user@ip-172-31-42-226.ec2.internal>
2020-05-08 10:41:09 +0000

    first commit

---
+++ src/main/java/com/monica/azure/deployjavaazure/controller/AzureController.java
@@ -0,0 +1,25 @@
+package com.monica.azure.deployjavaazure.controller;
+
+import java.net.InetAddress;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureController {
+    @RequestMapping(value="/gethost", method=RequestMethod.GET)
+	public String requestMethodName(final HttpServletRequest request) {
+        final String clientAddress=request.getLocalName();
+        try{
+            InetAddress serverAddress;
+            serverAddress= InetAddress.getLocalHost();
+            return "Request is comming from : "+clientAddress+" for server with hostname/ip : "+serverAddress;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+	}
+}
