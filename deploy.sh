df33bf54fcdb307cab00fd5ac4f3b8d44ebb00fa
moshinde <azagent@ip-172-31-85-137.ec2.internal>
2020-05-09 10:45:17 +0000

    Corrected unix format

--- deploy.sh
+++ deploy.sh
@@ -2,42 +2,46 @@
 SERVICE_NAME=deploy-java-azure
 PATH_TO_JAR=./deploy-java-azure.jar
 PID_PATH_NAME=/tmp/deploy-java-azure-pid
-case $1 in
-start)
-       echo "Starting $SERVICE_NAME ..."
-  if [ ! -f $PID_PATH_NAME ]; then
-       nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >>/dev/null &
-                   echo $! > $PID_PATH_NAME;
-        PID=$(cat $PID_PATH_NAME);
-       echo "$SERVICE_NAME started with PID $PID ..."
-  else
-       echo "$SERVICE_NAME is already running ..."
-  fi
-;;
-stop)
-  if [ -f $PID_PATH_NAME ]; then
-         PID=$(cat $PID_PATH_NAME);
-         echo "$SERVICE_NAME stoping ..."
-         kill $PID;
-         echo "$SERVICE_NAME stopped ..."
-         rm $PID_PATH_NAME
-  else
-         echo "$SERVICE_NAME is not running ..."
-  fi
-;;  
-restart)
-  if [ -f $PID_PATH_NAME ]; then
-      PID=$(cat $PID_PATH_NAME);   
-      echo "$SERVICE_NAME stopping ...";
-      kill $PID;
-      echo "$SERVICE_NAME stopped ...";
-      rm $PID_PATH_NAME
-      echo "$SERVICE_NAME starting ..."
-      nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-      echo $! > $PID_PATH_NAME
-      echo "$SERVICE_NAME started ..."
-  else
-      echo "$SERVICE_NAME is not running ..."
-     fi;;
- esac
-
+if [ -f "$PATH_TO_JAR" ]; then
+	case $1 in
+	start)
+		   echo "Starting $SERVICE_NAME ..."
+	  if [ ! -f $PID_PATH_NAME ]; then
+		   nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >>/dev/null &
+					   echo $! > $PID_PATH_NAME;
+			PID=$(cat $PID_PATH_NAME);
+		   echo "$SERVICE_NAME started with PID $PID ..."
+	  else
+		   echo "$SERVICE_NAME is already running ..."
+	  fi
+	;;
+	stop)
+	  if [ -f $PID_PATH_NAME ]; then
+			 PID=$(cat $PID_PATH_NAME);
+			 echo "$SERVICE_NAME stoping ..."
+			 kill $PID;
+			 echo "$SERVICE_NAME stopped ..."
+			 rm $PID_PATH_NAME
+	  else
+			 echo "$SERVICE_NAME is not running ..."
+	  fi
+	;;  
+	restart)
+	  if [ -f $PID_PATH_NAME ]; then
+		  PID=$(cat $PID_PATH_NAME);   
+		  echo "$SERVICE_NAME stopping ...";
+		  kill $PID;
+		  echo "$SERVICE_NAME stopped ...";
+		  rm $PID_PATH_NAME
+		  echo "$SERVICE_NAME starting ..."
+		  nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+		  echo $! > $PID_PATH_NAME
+		  echo "$SERVICE_NAME started ..."
+	  else
+		  echo "$SERVICE_NAME is not running ..."
+		 fi;;
+	 esac
+else
+    echo "$PATH_TO_JAR doesnt exist. Deployment will be terminated"
+    exit 2
+fi
