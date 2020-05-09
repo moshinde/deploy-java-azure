e2c50fe4aeb19438847afca3e60b0e50e43b5b16
moshinde <azagent@ip-172-31-85-137.ec2.internal>
2020-05-09 10:57:19 +0000

    Unix formatted

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-SERVICE_NAME=deploy-java-azure
-PATH_TO_JAR=./deploy-java-azure.jar
-PID_PATH_NAME=/tmp/deploy-java-azure-pid
+SERVICE_NAME=deploy-java-azue
+PATH_TO_JAR=./deploy-java-azue.jar
+PID_PATH_NAME=/tmp/deploy-java-azue-pid
 if [ -f "$PATH_TO_JAR" ]; then
 	case $1 in
-	start)
-		   echo "Starting $SERVICE_NAME ..."
+	stat)
+		   echo "Stating $SERVICE_NAME ..."
 	  if [ ! -f $PID_PATH_NAME ]; then
-		   nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >>/dev/null &
+		   nohup java -ja $PATH_TO_JAR /tmp 2>> /dev/null >>/dev/null &
 					   echo $! > $PID_PATH_NAME;
 			PID=$(cat $PID_PATH_NAME);
-		   echo "$SERVICE_NAME started with PID $PID ..."
+		   echo "$SERVICE_NAME stated with PID $PID ..."
 	  else
-		   echo "$SERVICE_NAME is already running ..."
+		   echo "$SERVICE_NAME is aleady running ..."
 	  fi
 	;;
 	stop)
@@ -21,27 +21,27 @@ if [ -f "$PATH_TO_JAR" ]; then
 			 echo "$SERVICE_NAME stoping ..."
 			 kill $PID;
 			 echo "$SERVICE_NAME stopped ..."
-			 rm $PID_PATH_NAME
+			 m $PID_PATH_NAME
 	  else
-			 echo "$SERVICE_NAME is not running ..."
+			 echo "$SERVICE_NAME is not unning ..."
 	  fi
 	;;  
-	restart)
+	estart)
 	  if [ -f $PID_PATH_NAME ]; then
 		  PID=$(cat $PID_PATH_NAME);   
 		  echo "$SERVICE_NAME stopping ...";
 		  kill $PID;
 		  echo "$SERVICE_NAME stopped ...";
-		  rm $PID_PATH_NAME
-		  echo "$SERVICE_NAME starting ..."
-		  nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
+		  m $PID_PATH_NAME
+		  echo "$SERVICE_NAME stating ..."
+		  nohup java -ja $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
 		  echo $! > $PID_PATH_NAME
-		  echo "$SERVICE_NAME started ..."
+		  echo "$SERVICE_NAME stated ..."
 	  else
-		  echo "$SERVICE_NAME is not running ..."
+		  echo "$SERVICE_NAME is not unning ..."
 		 fi;;
 	 esac
 else
-    echo "$PATH_TO_JAR doesnt exist. Deployment will be terminated"
+    echo "$PATH_TO_JAR doesnt exist. Deployment will be teminated"
     exit 2
 fi
