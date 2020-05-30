0bf7941f323610c60e35d200f47baba4f2db7a66
moshinde <31102278+moshinde@users.noreply.github.com>
2020-05-30 13:18:36 +0530

    Updated to add apache folder

--- provisionServer.sh
+++ provisionServer.sh
@@ -21,6 +21,11 @@ cat <<EOF > /etc/systemd/system/deploy-java-azure.service
 [Install]
  WantedBy=multi-user.target
 EOF
+mkdir /opt/apache/
+chown azagentuser: /opt/apache/
+chmod u+w /opt/apache/
+chown azagentuser: /usr/local/bin/
+chmod u+w /usr/local/bin/
 echo "Switching user"
 su -  azagentuser bash -c 'mkdir azagent;cd azagent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.168.2/vsts-agent-linux-x64-2.168.2.tar.gz;tar -zxvf vstsagent.tar.gz; if [ -x "$(command -v systemctl)" ]; then ./config.sh --deploymentgroup --deploymentgroupname "Java-service-DEV" --acceptteeeula --agent $HOSTNAME --addDeploymentGroupTags --deploymentGroupTags "dev" --url https://dev.azure.com/MonicaShinde1307/ --work _work --projectname 'RockNRollTrain' --auth PAT --token ***** --runasservice; sudo ./svc.sh install; sudo ./svc.sh start; else ./config.sh --deploymentgroup --deploymentgroupname "Java-service-DEV" --acceptteeeula --agent $HOSTNAME --addDeploymentGroupTags --deploymentGroupTags "dev" --url https://dev.azure.com/MonicaShinde1307/ --work _work --projectname 'RockNRollTrain' --auth PAT --token *****; ./run.sh; fi'
 echo "Done agent registration"
